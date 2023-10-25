3e5cb5072b80a607bc35bdeb28a5b8d7b3e8abf8
Pedro Pereira <Pedro.Pereira@wj.com.br>
2023-10-25 11:47:47 -0300

    Update Test

--- src/main/java/suporteTest/GerarBoletosSuporteTest.java
+++ src/main/java/suporteTest/GerarBoletosSuporteTest.java
@@ -2,6 +2,7 @@ package suporteTest;
 
 import core.LoginPage;
 import org.junit.Assert;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,10 +35,27 @@ public class GerarBoletosSuporteTest {
                 "                "));
     }
 
-    @Test
+    @Disabled
     public void consultarPlaca(){
+        escrever(By.id("placa"),"NOQ4J88");
+        clicarBotao(By.id("pesquisaPlaca"));
+        forceWait();
+        clicarBotao(By.xpath("/html/body/div[3]/div/div[10]/button[1]"));
+        forceWait();
+        clicarBotao(By.xpath("/html/body/div[3]/div/div[10]/button[1]"));
+
+        Assert.assertTrue(contemTexto("Boleto desvinculado com sucesso."));
+        Assert.assertTrue(contemTexto("A placa pesquisada já possui um boleto associado e não pago, deseja desvinculá-lo e continuar?"));
+        Assert.assertEquals(obterValorCampo("nome"),"ALISON HENRIQUE BATISTA");
+        Assert.assertEquals(obterValorCampo("loja"),"ORIGINAL PLACAS EIRELI");
+        Assert.assertEquals(obterValorCampo("valor"),"R$ 200,00");
+    }
+
+    @Test
+    public void consultarPlacaSemBoleto(){
         escrever(By.id("placa"),"QBL1A28");
-        clicarBotao(By.xpath("/html/body/div[2]/main/div/main/div/button"));
+        clicarBotao(By.id("pesquisaPlaca"));
+        forceWait();
 
         Assert.assertEquals(obterValorCampo("nome"),"JOSE ARTHUR FERNANDES COUTINHO");
         Assert.assertEquals(obterValorCampo("loja"),"VISTORIA VEICULAR VILA VELHA LTDA");
@@ -45,6 +63,26 @@ public class GerarBoletosSuporteTest {
     }
 
     @Test
+    public void tentativaDeConsultaSemPassarPlaca(){
+        escrever(By.id("placa"),"");
+        clicarBotao(By.id("pesquisaPlaca"));
+        forceWait();
+
+        Assert.assertTrue(contemTexto("Por favor, insira uma placa para prosseguir!."));
+    }
+
+    @Test
+    public void consultarPlacaComBoletoPago(){
+        escrever(By.id("placa"),"LDV4H70");
+        clicarBotao(By.id("pesquisaPlaca"));
+        forceWait();
+
+        Assert.assertTrue(contemTexto("A placa pesquisada já possui um boleto "));
+        Assert.assertTrue(contemTexto(" associado e consta como PAGO. "));
+        Assert.assertTrue(contemTexto(" Por favor, busque por outra placa."));
+    }
+
+    @Disabled
     public void TentativacriarBoletoComCamposVazios(){
         escrever(By.id("placa"),"QBL1A28");
         clicarBotao(By.xpath("/html/body/div[2]/main/div/main/div/button/span"));
